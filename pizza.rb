4b6f1e492dd1048d2ee761193a70ba1d93db028c
Lynda <lyndawellhausen@Gmail.com>
2013-09-11 14:25:57 -0500

    updated initialize to contain vegetarian boolean

--- pizza.rb
+++ pizza.rb
@@ -4,15 +4,17 @@ module Pizza
   end
 
   class Topping
-    attr_accessor :name
-    
+    attr_accessor :name, :vegetarian
+
     # Instantiate a new topping.
     #
     # name - the String name of the topping.
+    # vegetarian - boolean value for vegetarian.
     #
     # Returns a new Topping object.
-    def initialize(name)
+    def initialize(name, vegetarian: false)
       @name = name
+      @vegetarian = vegetarian
     end
   end
 end
